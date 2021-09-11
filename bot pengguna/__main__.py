1dbf6e60a5abd0452b319e5ab7551962dcf6a92b
Duta <86175519+Masduta@users.noreply.github.com>
2021-09-11 15:29:40 +0700

    Update and rename userbot/__main__.py to bot pengguna/__main__.py

--- bot pengguna/__main__.py
+++ bot pengguna/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f"👻 Duta-Xbot 👻 ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
+    f"👻Duta-Xbot👻 ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
 
 
 if len(argv) not in (1, 3, 4):
